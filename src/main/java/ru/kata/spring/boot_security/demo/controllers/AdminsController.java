90293004349c5a3df888605b19b24c299b777e0b
Dima Rumynskii <ra-z88@mail.ru>
2023-03-20 17:19:42 +0300

    Second commit

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminsController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminsController.java
@@ -5,36 +5,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
 import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
-public class MyAdminUserController {
+public class AdminsController {
+
     private final UserService userService;
 
-    @Autowired
-    public MyAdminUserController(UserService userService) {
-        this.userService = userService;
-    }
+    private final RoleService roleService;
 
-    @GetMapping("/user")
-    public String showUser(Model model, Principal principal) {
-        model.addAttribute("user", userService.findByUsername(principal.getName()));
-        return "user";
+    @Autowired
+    public AdminsController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
     }
 
     @GetMapping("/")
-    public String showAll(Model model, Principal principal) {
+    public String showAllUsers(Model model, Principal principal) {
         model.addAttribute("user", userService.findByUsername(principal.getName()));
         model.addAttribute("users", userService.findAll());
         model.addAttribute("userNew", new User());
+        model.addAttribute("allRoles", roleService.findAll());
         return "admin";
     }
 
     @PatchMapping("admin/{id}")
-    public String edit(@ModelAttribute User user, @PathVariable("id") Long id) {
+    public String update(@ModelAttribute User user, @PathVariable("id") Long id) {
         userService.update(id, user);
         return "redirect:/";
     }
